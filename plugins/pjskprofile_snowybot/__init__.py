10c17ccf25276f11a05d2c1dac42e31c62aa1e78
HakuchumuHYX <thefirechaser@gmail.com>
2026-03-22 10:56:37 +0800

    永远怀念

--- plugins/pjskprofile_snowybot/__init__.py
+++ plugins/pjskprofile_snowybot/__init__.py
@@ -1,7 +1,10 @@
 from nonebot import require
 
-require("nonebot_plugin_localstore")
+# 已废弃：上游项目停止维护，停用该插件
+# 再见了，所有的snowybot
+# 你好，所有的moesekai
+# require("nonebot_plugin_localstore")
 
-# 导入功能模块
-from . import bind
-from . import profile
+# 导入功能模块（已停用）
+# from . import bind
+# from . import profile
